7c0022c17a7ca51063ebcaf8648af089ad80cae2
Sira <admin@sira.software>
2026-05-24 14:08:22 +0300

    fix: apply final cap-setup automation script and configurations from patch 888

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
+import tailwindcss  from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
-  plugins: {
-    tailwindcss:  { config: './apps/web/tailwind.config.js' },
-    autoprefixer: {
-      overrideBrowserslist: ['android >= 5', 'chrome >= 67', 'ios_saf >= 12'],
-    },
-  },
+  plugins: [
+    tailwindcss('./apps/web/tailwind.config.js'),
+    autoprefixer({
+      overrideBrowserslist: ['android >= 5', 'chrome >= 67'],
+    }),
+  ],
 };
